a0d6d8ae204e7a23c7b26983084b56a770af05c9
MayGrass <15877196+MayGrass@users.noreply.github.com>
2019-12-10 16:50:02 +0800

    QRCODE & show result in log.v

---
+++ app/src/main/java/tw/org/iii/android_qrcode/ScanActivity.java
@@ -0,0 +1,43 @@
+package tw.org.iii.android_qrcode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class ScanActivity extends AppCompatActivity  implements ZXingScannerView.ResultHandler{
+    private ZXingScannerView mScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_scan);
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+
+        String result = rawResult.getText();
+        Log.v("DCH", result);
+        mScannerView.resumeCameraPreview(this);
+    }
+}
